7241edbdca8ff707c34d704ecc0fa6c692a390a6
Zsolt Botykai <zsolt.botykai@ericsson.com>
2017-01-21 00:01:33 +0100

    Actual conversion enabled.

--- GETES.sh
+++ GETES.sh
@@ -9,27 +9,27 @@ week=$(echo $AKT_LAPSZAM | sed 's/.*id="lapszamvalaszto">[^ ]\+\.[^,]\+, \([0-9]
 ### print debugger
 ### echo $year $week
 
-## ebook-convert es.recipe es_${year}-${week}.mobi \
-##               --output-profile kindle \
-##               --smarten-punctuation \
-##               --change-justification justify \
-##               --title "Élet és Irodalom LXI-3" \
-##               --toc-title "Tartalom" -vvvvvv \
-##               --remove-paragraph-spacing-indent-size 1.3 \
-##               --remove-paragraph-spacing \
-##               --sr1-search ".A vers olvas.s.hoz, k.rj.k, fizessen el..." \
-##               --sr1-replace ""
-## ebook-convert es_${year}-${week}.mobi es_L${year}-${week}.pdf \
-##               --extra-css "body  {background-color: white; color: black; }" \
-##               --paper-size a4 \
-##               --smarten-punctuation \
-##               --change-justification justify \
-##               --title "Élet és Irodalom LXI-3" \
-##               --toc-title "Tartalom" \
-##               --pretty-print \
-##               --insert-blank-line \
-##               --margin-bottom 50.0 \
-##               --margin-top 50.0 \
-##               --margin-left 50.0 \
-##               --margin-right 50.0
-## 
+ebook-convert es.recipe es_${year}-${week}.mobi \
+              --output-profile kindle \
+              --smarten-punctuation \
+              --change-justification justify \
+              --title "Élet és Irodalom ${year}. ${week}. szám" \
+              --toc-title "Tartalom" -vvvvvv \
+              --remove-paragraph-spacing-indent-size 1.3 \
+              --remove-paragraph-spacing \
+              --sr1-search ".A vers olvas.s.hoz, k.rj.k, fizessen el..." \
+              --sr1-replace ""
+ebook-convert es_${year}-${week}.mobi es_L${year}-${week}.pdf \
+              --extra-css "body  {background-color: white; color: black; }" \
+              --paper-size a4 \
+              --smarten-punctuation \
+              --change-justification justify \
+              --title "Élet és Irodalom ${year}. ${week}. szám" \
+              --toc-title "Tartalom" \
+              --pretty-print \
+              --insert-blank-line \
+              --margin-bottom 50.0 \
+              --margin-top 50.0 \
+              --margin-left 50.0 \
+              --margin-right 50.0
+
