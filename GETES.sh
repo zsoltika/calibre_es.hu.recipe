465267072f709089c7a8e06497162ad1418f58a1
Zsolt Botykai <zsolt.botykai@gmail.com>
2017-03-24 09:07:21 +0100

    The handling of dates/issues had changed.

--- GETES.sh
+++ GETES.sh
@@ -3,18 +3,20 @@
 # Get the annual weekly id of the newspaper
 
 AKT_LAPSZAM=$(curl -s www.es.hu | fgrep '"lapszamvalaszto"')
-year=$(echo $AKT_LAPSZAM | sed 's/.*id="lapszamvalaszto">\([^ ]\+\)\..*/\1/')
-week=$(echo $AKT_LAPSZAM | sed 's/.*id="lapszamvalaszto">[^ ]\+\.[^,]\+, \([0-9]\+\)\..*/\1/')
+year=$(echo $AKT_LAPSZAM | sed 's/.*id="lapszamvalaszto">[ \t]*\([^ ]\+\)\..*/\1/')
+week=$(echo $AKT_LAPSZAM | sed 's/.*.vfolyam,[ \t]\+\([0-9]\+\)\.[ \t]sz.m.*/\1/')
 
-### print debugger
-### echo $year $week
+## DEBUG
+# echo $year 
+# echo $week
+# exit 0
 
 ebook-convert es.recipe es_${year}-${week}.mobi \
               --output-profile kindle \
               --smarten-punctuation \
               --change-justification justify \
               --title "Élet és Irodalom ${year}. ${week}. szám" \
-              --toc-title "Tartalom" -vvvvvv \
+              --toc-title "Tartalom" \
               --remove-paragraph-spacing-indent-size 1.3 \
               --remove-paragraph-spacing \
               --sr1-search ".A vers olvas.s.hoz, k.rj.k, fizessen el..." \
