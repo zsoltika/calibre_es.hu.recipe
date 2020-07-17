bdec134d3674e2d6ef09f6209d4bc6a2945aafb4
Zsolt Botykai <zsolt.botykai@gmail.com>
2020-07-17 07:38:13 +0200

    no PDF, local cover

--- GETES.sh
+++ GETES.sh
@@ -20,22 +20,23 @@ ebook-convert es.recipe es_${year}-${week}.mobi \
               --remove-paragraph-spacing-indent-size 1.3 \
               --remove-paragraph-spacing \
               --sr1-search ".A vers olvas.s.hoz, k.rj.k, fizessen el..." \
-              --sr1-replace ""
-ebook-convert es_${year}-${week}.mobi es_${year}-${week}.pdf \
-              --extra-css "body  {background-color: white; color: black; }" \
-              --paper-size a4 \
-              --smarten-punctuation \
-              --change-justification justify \
-              --title "Élet és Irodalom ${year}. ${week}. szám" \
-              --toc-title "Tartalom" \
-              --pretty-print \
-              --insert-blank-line \
-              --remove-paragraph-spacing-indent-size 1.3 \
-              --remove-paragraph-spacing \
-              --margin-bottom 50.0 \
-              --margin-top 50.0 \
-              --margin-left 50.0 \
-              --margin-right 50.0 && \
+              --sr1-replace "" \
+              --cover es_cover.jpg
+### ebook-convert es_${year}-${week}.mobi es_${year}-${week}.pdf \
+###               --extra-css "body  {background-color: white; color: black; }" \
+###               --paper-size a4 \
+###               --smarten-punctuation \
+###               --change-justification justify \
+###               --title "Élet és Irodalom ${year}. ${week}. szám" \
+###               --toc-title "Tartalom" \
+###               --pretty-print \
+###               --insert-blank-line \
+###               --remove-paragraph-spacing-indent-size 1.3 \
+###               --remove-paragraph-spacing \
+###               --margin-bottom 50.0 \
+###               --margin-top 50.0 \
+###               --margin-left 50.0 \
+###               --margin-right 50.0 
 echo "COPYING SOME FILEZ" && \
 cp es_${year}-${week}.* /mnt/c/Users/ebotzso/Documents && \
 echo "AND done...."
